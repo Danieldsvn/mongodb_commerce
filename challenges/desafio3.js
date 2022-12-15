d824074779339c98d3020ee0c8955778da242736
Daniel Saraiva <danieldsvn@gmail.com>
2022-12-15 16:06:12 -0300

    lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,3 @@
 db.produtos.find({}, {
   nome: 1, vendidos: 1, _id: 0,
-}).sort({ vendidos: -1}).limit(1);
+}).sort({ vendidos: -1 }).limit(1);
--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -2,4 +2,4 @@ db.produtos.find({
   vendidos: { $gt: 50, $lt: 100 },  
 }, {
   nome: 1, vendidos: 1, _id: 0,
-}).sort({ vendidos: 1});
+}).sort({ vendidos: 1 });
